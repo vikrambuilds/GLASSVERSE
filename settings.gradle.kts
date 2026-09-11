ceb660de01869f693dba9d273767e5f6ade7a30d
vikrambuils <vikrambuils@users.noreply.github.com>
2026-09-11 13:21:35 +0530

    Update dependency resolution management in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
